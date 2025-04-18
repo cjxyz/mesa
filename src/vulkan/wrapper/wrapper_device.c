1b5222b14ddfc0aefdc630a48f369961436f90b2
Pipetto-crypto <Pipetto-crypto@pipetto-crypto.com>
2025-04-18 12:25:14 +0200

    wrapper: fix vkd3d not working on turnip on some Adreno devices

--- src/vulkan/wrapper/wrapper_device.c
+++ src/vulkan/wrapper/wrapper_device.c
@@ -21,8 +21,10 @@ const struct vk_device_extension_table wrapper_device_extensions =
 #endif
    .KHR_present_id = true,
    .KHR_present_wait = true,
+   .KHR_dynamic_rendering = true,
    .KHR_incremental_present = true,
    .EXT_map_memory_placed = true,
+   .KHR_maintenance4 = true,
    .KHR_map_memory2 = true,
 };
 
